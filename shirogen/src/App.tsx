bd127d7febcb39944ef1cf84bfe482a6a4e8fd90
Jonas Mueller <jonas.mueller-1@uni-hamburg.de>
2025-05-15 22:51:41 +0200

    Added basic japanese functionality

--- shirogen/src/App.tsx
+++ shirogen/src/App.tsx
@@ -4,17 +4,33 @@ import MainGermanQuiz from "./components/MainGermanQuiz"
 import MainJapaneseQuiz from './components/MainJapaneseQuiz';
 
 const QuizHandler = () => {
-    const variable = false
-    return (
-        <>
-            { variable ? ( 
-                <MainGermanQuiz />
-            ) : (
-                <MainJapaneseQuiz />
-            )
-        }
-        </>
-    );
-}
+  const [selectedQuiz, setSelectedQuiz] = useState("german");
+
+  return (
+    <>
+      <div className="quiz-toggle-bar">
+        <button
+            className={`quiz-toggle-button ${selectedQuiz === "german" ? "active" : ""}`}
+            onClick={() => setSelectedQuiz("german")}
+        >
+            German
+        </button>
+        <button
+            className={`quiz-toggle-button ${selectedQuiz === "japanese" ? "active" : ""}`}
+            onClick={() => setSelectedQuiz("japanese")}
+        >
+            Japanese
+        </button>
+      </div>
+
+      {selectedQuiz === "german" ? (
+        <MainGermanQuiz />
+      ) : (
+        <MainJapaneseQuiz />
+      )}
+    </>
+  );
+};
+
 
 export default QuizHandler;
